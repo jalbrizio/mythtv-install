0033f8ca14c7e84389b3848aa53fcc9d231ae283
Jeremi Albrizio <jalbrizio@gmail.com>
2009-04-28 15:33:25 -0700

    mythtv repos and files to convert to flash video.

---
+++ test3.php
@@ -0,0 +1,24 @@
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  
+	<!-- page title -->
+	<title>Now Playing xxxxx</title>
+
+</head><body>
+
+				<div class="skinpreview"><script type="text/javascript" src="swfobject.js"></script>
+				<div name="mediaspace" id="mediaspace">
+				<script type="text/javascript">
+					var so = new SWFObject('player.swf','single','100%','100%','0');
+					so.addVariable('fullscreen','true');
+					so.addParam("allowfullscreen","true");
+					so.addParam("allowscriptaccess','always");
+					so.addParam("wmode","opaque");
+					so.addVariable("file", "../flmovies/yyyyy");
+					so.addVariable("image", "");
+					so.addVariable("skin", "controlpanel.swf");
+					so.write('mediaspace');
+				</script></div></div>
+
+	
+</body></html>
